f9a605922d5a74b806ac3127798983b7dee1d24a
Niubfinance <chelseazhao99@gmail.com>
2021-05-10 21:20:24 +0800

    coming soon

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <img class="logo" src="./assets/logo.svg">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/Home.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+body {
+    margin: auto;
+    background: #080c13;
+    font-size: 16px;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.logo {
+    width: 229px;
+    height: auto;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="hello">
+        <p>
+            NIUB is a decentralized token fully autonomous by the community.<br>
+            NIUB comes from the Chinese homophonic "niubi". which means very powerful and sturdy.
+            <a href="https://twitter.com/NiubFinance">Join Airdrop</a>
+        </p>
+        <h1>Coming soon!</h1>
+
+        <h3>Essential Links</h3>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank" rel="noopener">GitHub</a></li>
+            <li><a href="https://twitter.com/NiubFinance" target="_blank" rel="noopener">Twitter</a></li>
+            <li><a href="https://t.me/joinchat/Cfhe_kjXFsA2YjNh" target="_blank" rel="noopener">Telegram</a></li>
+<!--            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Discord</a></li>-->
+
+        </ul>
+        <h3>Ecosystem</h3>
+        <ul>
+            <li><a href="https://www.binance.org/" target="_blank" rel="noopener">BSC-Chain</a></li>
+            <li><a href="https://bscscan.com/address/0xb65bec0f80f29bfd53d4d5b328a1aa313688ddc2" target="_blank" rel="noopener">Token</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #cc2141;
+}
+</style>
